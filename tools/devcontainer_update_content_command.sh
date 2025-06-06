ba8ad9de95198b89439ea084b236779c928b67c0
Isamu Mogi <isamu@leafytree.jp>
2025-06-07 05:47:19 +0900

    chmod +x

--- tools/devcontainer_update_content_command.sh
+++ tools/devcontainer_update_content_command.sh
@@ -6,5 +6,4 @@ set -eu -o pipefail
 cd "$(dirname "$0")/.."
 
 sudo chown -R "$(id -u):$(id -g)" .
-sudo find . -type d -exec chmod 755 {} +
-sudo find . -type f -exec chmod 644 {} +
+find . -type f -name "*.sh" -exec chmod +x {} +
